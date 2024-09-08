26c2b98ecbbe09905b761271166f2517934bcaae
Dhruv Yelmame <135983534+DhruvYelmame@users.noreply.github.com>
2024-09-08 10:26:43 +0530

    Add files via upload

---
+++ music_library.py
@@ -0,0 +1,5 @@
+music = {
+    "zingaat" : "https://youtu.be/2gcsgfzqN8k?si=5In-Rgvno0P2E82t",
+    "munni" : "https://youtu.be/Jn5hsfbhWx4?si=Yh1US07A8aj2JAiB",
+    "fevicol" : "https://youtu.be/zE7Pwgl6sLA?si=3tD9rgtg4YL3rumM"
+}
